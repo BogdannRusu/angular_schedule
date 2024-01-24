9c2930bc282adfbdd5eb931a491366202b1f1059
bogdanrusu <bogdanrusu788@gmail.com>
2024-01-24 13:18:53 +0200

    a lot of changing in front of schedule dashboard

--- web_app/src/app/app.component.ts
+++ web_app/src/app/app.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { ScheduleModule, RecurrenceEditorModule, DayService, WeekService,WorkWeekService,MonthService, MonthAgendaService } from '@syncfusion/ej2-angular-schedule';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LoginComponent],
+  imports: [
+    RouterOutlet, 
+    LoginComponent,
+    ScheduleModule,
+    RecurrenceEditorModule
+  ],
+  providers: [DayService, WeekService, WorkWeekService, MonthService, MonthAgendaService],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
