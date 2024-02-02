de4c82c5f7826678197fd1c6bc4c35f221fa98be
bogdan.rusu <bogdanvoevod722@gmail.com>
2024-02-02 17:57:00 +0200

    adding some changes and add main component

--- schedule_web/src/app/app.module.ts
+++ schedule_web/src/app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppComponent } from './app-components/app.component';
 import { LoginComponent } from './login/login.component';
 import { HeaderComponent } from './header/header.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    HeaderComponent
+    HeaderComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule
