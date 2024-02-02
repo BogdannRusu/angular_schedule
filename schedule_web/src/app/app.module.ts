965bdfc7f9f6d6680a0e403fcb1f33dd587764f7
bogdanrusu <bogdanrusu788@gmail.com>
2024-02-02 15:58:30 +0200

    solving error and removing table component

--- schedule_web/src/app/app.module.ts
+++ schedule_web/src/app/app.module.ts
@@ -1,15 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
-
 import { AppComponent } from './app-components/app.component';
 import { LoginComponent } from './login/login.component';
-import { TableSchedulerComponent } from './table-scheduler/table-scheduler.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    TableSchedulerComponent
+    HeaderComponent
   ],
   imports: [
     BrowserModule
--- schedule_web/src/app/header/header.component.spec.ts
+++ schedule_web/src/app/header/header.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { TableSchedulerComponent } from './table-scheduler.component';
+import { HeaderComponent } from './header.component';
 
-describe('TableSchedulerComponent', () => {
-  let component: TableSchedulerComponent;
-  let fixture: ComponentFixture<TableSchedulerComponent>;
+describe('HeaderComponent', () => {
+  let component: HeaderComponent;
+  let fixture: ComponentFixture<HeaderComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [TableSchedulerComponent]
+      declarations: [HeaderComponent]
     })
     .compileComponents();
     
-    fixture = TestBed.createComponent(TableSchedulerComponent);
+    fixture = TestBed.createComponent(HeaderComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
